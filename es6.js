d5deffd701c04256111b85a7caebf9eec006e9ae
Abrar-0 <abrarzawad222@gmail.com>
2024-03-07 20:39:54 +0600

    wow

---
+++ es6.js
@@ -0,0 +1,12 @@
+const student = {
+    name : "abrar zawad",
+    age:24,
+    graduated: true,
+    numbers:[24,43,34]
+}
+const age = [23,563,55,2,5]
+
+
+// template string
+
+const about = `My name is `
